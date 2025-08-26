48db832e98d71bd1f4ec1ebd760966cd8a2b6631
SaadiYousaf <saadsaqib869@gmail.com>
2025-08-26 20:25:47 +0500

    RolesUpdated

--- src/pages/UserProfile/ProfilePage.js
+++ src/pages/UserProfile/ProfilePage.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchUserProfile, fetchUserOrders } from '../../store/profileSlice';
-
+import './ProfilePage.css';
 
 const ProfilePage = () => {
   const dispatch = useDispatch();
